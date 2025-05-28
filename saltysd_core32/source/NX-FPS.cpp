656c68dc6f6b52c5f77c1b209320496f881a36cb
MasaGratoR <masagrator@gmail.com>
2025-05-28 18:00:42 +0200

    Add files via upload

--- saltysd_core32/source/NX-FPS.cpp
+++ saltysd_core32/source/NX-FPS.cpp
@@ -1028,7 +1028,7 @@ extern "C" {
 				uint8_t temp = 0;
 				SaltySDCore_fread(&temp, 1, 1, file_dat);
 				(Shared -> FPSlocked) = temp;
-				if (temp >= 40 && temp <= 75) {
+				if (temp >= 40 && temp <= 120) {
 					FILE* sync_file = SaltySDCore_fopen("sdmc:/SaltySD/flags/displaysync.flag", "rb");
 					if  (sync_file) {
 						SaltySDCore_fclose(sync_file);
