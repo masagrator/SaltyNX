505643f06c020d729a283e58073d243380f00c00
MasaGratoR <masagrator@gmail.com>
2025-09-06 22:12:30 +0200

    Add files via upload

--- saltysd_core32/source/NX-FPS.cpp
+++ saltysd_core32/source/NX-FPS.cpp
@@ -427,7 +427,7 @@ namespace NX_FPS_Math {
 	template <typename T> void addResToViewports(T m_width, T m_height) {
 		if (m_height <= (T)160) return;
 		T ratio = (m_width * 10) / m_height;
-		if (ratio >= (T)12 && ratio <= (T)18) {
+		if (ratio >= (T)6 && ratio <= (T)18) {
 			union {
 				struct {
 					uint16_t width;
