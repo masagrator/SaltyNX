a53d29b32ade61904a3ad1163951af8954139ae0
MasaGratoR <masagrator@gmail.com>
2025-09-10 21:53:11 +0200

    Add files via upload

--- saltysd_proc/source/display_refresh_rate.cpp
+++ saltysd_proc/source/display_refresh_rate.cpp
@@ -216,29 +216,26 @@ extern "C" __attribute__((noinline)) void correctOledGamma(uint32_t refresh_rate
     i = 0;
     #define loop_amount 5
     
-    uint32_t offsets[] = {0x1A, 0x24, 0x25, 0x3D};
-    uint32_t values_set[4] = {2, 0, 0x83, 0};
+    uint32_t offsets[] = {0x1A, 0x24, 0x25};
+    uint32_t values_set[4] = {2, 0, 0x83};
     if (refresh_rate == 60) {
         if (last_refresh_rate == 60) return;
     }
     else if (refresh_rate == 45) {
         if (last_refresh_rate == 45) return;
-        uint32_t values[4] = {4, 1, 0, 3};
-        if (file_or_directory_exists("sdmc:/SaltySD/flags/oled_maxbrightness.flag")) values[3] = 0;
+        uint32_t values[4] = {4, 1, 0};
         memcpy(values_set, values, 16);
 
     }
     else if (refresh_rate == 50) {
         if (last_refresh_rate == 50) return;
-        uint32_t values[4] = {4, 1, 0, 2};
-        if (file_or_directory_exists("sdmc:/SaltySD/flags/oled_maxbrightness.flag")) values[3] = 0;
+        uint32_t values[4] = {4, 1, 0};
         memcpy(values_set, values, 16);
   
     }
     else if (refresh_rate == 55) {
         if (last_refresh_rate == 55) return;
-        uint32_t values[4] = {3, 1, 0, 2};
-        if (file_or_directory_exists("sdmc:/SaltySD/flags/oled_maxbrightness.flag")) values[3] = 0;
+        uint32_t values[4] = {3, 1, 0};
         memcpy(values_set, values, 16);
     }
     else return;
