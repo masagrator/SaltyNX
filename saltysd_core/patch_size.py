4303fb49df0af621f1db2fa6f2a85b240abbc4f8
MasaGratoR <masagrator@gmail.com>
2025-11-05 20:29:21 +0100

    Update patch_size.py

--- saltysd_core/patch_size.py
+++ saltysd_core/patch_size.py
@@ -6,11 +6,13 @@ file = open("size.txt", "r")
 size = int(file.read(), base=10)
 file.close()
 
-deadbeef = DATA.find(b"\xEF\xBE\xAD\xDE\x00\x00\x00\x00")
+deadbeef_bytes = 0xDEADBEEF.to_bytes(8, "little")
+
+deadbeef = DATA.find(deadbeef_bytes)
 if (deadbeef == -1):
     raise ValueError("MAGIC size was not found!")
 encoded_size = size.to_bytes(8, "little")
-NEW_DATA = DATA.replace(b"\xEF\xBE\xAD\xDE\x00\x00\x00\x00", encoded_size, deadbeef)
+NEW_DATA = DATA.replace(deadbeef_bytes, encoded_size)
 
 file = open("saltysd_core.elf", "wb")
 file.write(NEW_DATA)
