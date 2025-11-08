261fbf00ca21f33cb54e3950f0476edf1177c0dd
MasaGratoR <masagrator@gmail.com>
2025-11-08 22:24:18 +0100

    Delete saltysd_core32 directory

--- saltysd_core32/source/bm.h
+++
@@ -1,8 +0,0 @@
-#ifndef BM_H
-#define BM_H
-
-// highly optimized data search, requires (256)+(4*patlen) stack 
-// returns NULL if pat not found
-void* boyer_moore_search(void *string, int stringlen, void *pat, int patlen);
-
-#endif /* BM_H */
