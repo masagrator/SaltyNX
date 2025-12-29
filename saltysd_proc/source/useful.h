e4412efe248811812a81fe1dfeaa596fef08b590
MasaGratoR <masagrator@gmail.com>
2025-12-29 23:01:03 +0100

    Revert last commit

--- saltysd_proc/source/useful.h
+++ saltysd_proc/source/useful.h
@@ -83,13 +83,46 @@ static inline bool file_or_directory_exists(const char *filename)
     struct stat buffer;
     return stat(filename, &buffer) == 0 ? true : false;
 }
-#ifdef __cplusplus
-extern "C" {
-#endif
-void SaltySD_printf(const char* format, ...);
-#ifdef __cplusplus
+
+static inline void SaltySD_printf(const char* format, ...)
+{
+	FILE* logflag = fopen("sdmc:/SaltySD/flags/log.flag", "r");
+	if (logflag == NULL) return;
+	fclose(logflag);
+	
+	char buffer[256];
+
+	va_list args;
+	va_start(args, format);
+	npf_vsnprintf(buffer, 256, format, args);
+	va_end(args);
+	
+	svcOutputDebugString(buffer, strlen(buffer));
+	
+	static bool previous_line_had_endline = false;
+	FILE* f = fopen("sdmc:/SaltySD/saltysd.log", "ab");
+	if (f)
+	{
+		static uint64_t tick = 0;
+		if (!tick) {
+			tick = svcGetSystemTick();
+		}
+		else if (previous_line_had_endline) {
+			char timer[] = "[244444444:24:24] ";
+			uint64_t deltaTick = svcGetSystemTick() - tick;
+			uint64_t deltaSeconds = deltaTick / systemtickfrequency;
+			npf_snprintf(timer, sizeof(timer), "[%02ld:%02ld:%02ld] ", (deltaSeconds/3600) % 1000000000, ((deltaSeconds/60) % 60), deltaSeconds % 60);
+			fwrite(timer, strlen(timer), 1, f);
+		}
+		if (buffer[strlen(buffer)-1] == '\n') {
+			previous_line_had_endline = true;
+		}
+		else previous_line_had_endline = false;
+		fwrite(buffer, strlen(buffer), 1, f);
+		fclose(f);
+	}
 }
-#endif
+
 
 #define debug_log(...) \
 	{char log_buf[0x200]; npf_snprintf(log_buf, 0x200, __VA_ARGS__); \
