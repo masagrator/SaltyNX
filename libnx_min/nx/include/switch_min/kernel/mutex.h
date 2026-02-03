9e9292b399dff7ab18be0e115b785f58b9865936
MasaGratoR <masagrator@gmail.com>
2026-02-03 09:48:59 +0100

    Update mutex.h

--- libnx_min/nx/include/switch_min/kernel/mutex.h
+++ libnx_min/nx/include/switch_min/kernel/mutex.h
@@ -10,8 +10,6 @@
 
 /// Mutex datatype, defined in newlib.
 typedef _LOCK_T Mutex;
-/// Recursive mutex datatype, defined in newlib.
-typedef _LOCK_RECURSIVE_T RMutex;
 
 /**
  * @brief Initializes a mutex.
@@ -41,34 +39,3 @@ bool mutexTryLock(Mutex* m);
  * @param m Mutex object.
  */
 void mutexUnlock(Mutex* m);
-
-/**
- * @brief Initializes a recursive mutex.
- * @param m Recursive mutex object.
- * @note A recursive mutex can also be statically initialized by assigning {0,0,0} to it.
- */
-static inline void rmutexInit(RMutex* m)
-{
-    m->lock = 0;
-    m->thread_tag = 0;
-    m->counter = 0;
-}
-
-/**
- * @brief Locks a recursive mutex.
- * @param m Recursive mutex object.
- */
-void rmutexLock(RMutex* m);
-
-/**
- * @brief Attempts to lock a recursive mutex without waiting.
- * @param m Recursive mutex object.
- * @return 1 if the mutex has been acquired successfully, and 0 on contention.
- */
-bool rmutexTryLock(RMutex* m);
-
-/**
- * @brief Unlocks a recursive mutex.
- * @param m Recursive mutex object.
- */
-void rmutexUnlock(RMutex* m);
